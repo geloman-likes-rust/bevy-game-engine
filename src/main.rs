68de8c022f943be32777e8c2be487c81364ddeeb
geloman-likes-rust <geloman-likes-rust@users.noreply.github.com>
2023-04-27 12:24:45 +0800

    Add player controller

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
+        .add_system(player_movement)
         .run();
 }
 
@@ -39,6 +40,40 @@ pub fn spawn_camera(
             ..default()
         }
     );
+}
+
+pub const PLAYER_SPEED: f32 = 250.0;
+
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let is_moving_left = keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A);
+        let is_moving_right = keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D);
+        let is_moving_down = keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S);
+        let is_moving_up = keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W);
+
+        let mut direction = Vec3::ZERO;
+
+        if is_moving_left {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+        if is_moving_right {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if is_moving_down {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+        if is_moving_up {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
 }
 
 #[derive(Component)]
